5866e1d670511628df493ae2bf6026c6d5589430
Reyhan Arighy <arighymoch@gmail.com>
2023-09-25 22:50:34 +0700

    Update adjustViewport.js

--- js/adjustViewport.js
+++ js/adjustViewport.js
@@ -1,22 +1,22 @@
-function isTouchDevice() {
-    return 'ontouchstart' in window || navigator.maxTouchPoints;
-}
+// function isTouchDevice() {
+    // return 'ontouchstart' in window || navigator.maxTouchPoints;
+// }
 
-function adjustViewportWidth() {
-    if (isTouchDevice()) {
-        document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=764, initial-scale=1.0');
-    } else {
-        document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0');
-    }
-}
+// function adjustViewportWidth() {
+    // if (isTouchDevice()) {
+        // document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=764, initial-scale=1.0');
+    // } else {
+        // document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0');
+    // }
+// }
 
 // Function to check and reload the page if necessary
-function checkAndReload() {
-    const currentWidth = window.innerWidth;
-    if ((initialWidth <= 764 && currentWidth > 764) || (initialWidth > 764 && currentWidth <= 764)) {
-        window.location.reload();
-    }
-}
+// function checkAndReload() {
+    // const currentWidth = window.innerWidth;
+    // if ((initialWidth <= 764 && currentWidth > 764) || (initialWidth > 764 && currentWidth <= 764)) {
+        // window.location.reload();
+    // }
+// }
 
 // Call the function on page load
 // window.addEventListener('load', () => {
