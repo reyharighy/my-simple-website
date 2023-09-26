5eda0f5fec7d1913c9a5ba9f2e81de967ffec355
Reyhan Arighy <arighymoch@gmail.com>
2023-09-26 18:44:06 +0700

    Update adjustViewport.js

--- js/adjustViewport.js
+++ js/adjustViewport.js
@@ -1,6 +1,6 @@
 function adjustViewportWidth(initialWidth) {
     if (initialWidth < 765) {
-        document.querySelector('meta[name="viewport"]').setAttribute('content', `width=764, initial-scale=${764/initialWidth}`);
+        document.querySelector('meta[name="viewport"]').setAttribute('content', `width=764, initial-scale=${initialWidth/764}`);
     } else {
         document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=device-width, initial-scale=1.0');
     }
